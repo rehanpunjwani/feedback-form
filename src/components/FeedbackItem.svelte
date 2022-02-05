6e94c418a88ea46886f8abd8053e89f92b52b5d3
Rehan Rehman <49568477+rehanpunjwani@users.noreply.github.com>
2022-02-05 16:43:16 +0500

    feat: :rocket: feedback components created with syle

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,41 @@
+<script>
+import Card from "./Card.svelte";
+export let item;
+</script>
+
+<Card>
+    <div class="num-display">
+        {item.rating}
+    </div>
+    <button class="close">
+        X
+    </button>
+    <div class="text-display">
+        {item.text}
+    </div>
+</Card>
+
+<style>
+.num-display {
+    position: absolute;
+    top: -10px;
+    left: -10px;
+    width: 50px;
+    height: 50px;
+    background: #246A73;
+    color: #f7d1b5;
+    border: 1px #f7d1b5 solid;
+    border-radius: 50%;
+    padding: 10px;
+    text-align: center;
+    font-size: 19px;
+  }
+  .close {
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+export let feedback =[]
+import FeedbackItem from "./FeedbackItem.svelte";
+
+</script>
+{#each feedback as fb (fb.id) }
+    <FeedbackItem item={fb} />
+{/each}
