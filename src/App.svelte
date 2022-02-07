cfb92ecff5e1155f23801cfad41255b612b2896f
Rehan Rehman <49568477+rehanpunjwani@users.noreply.github.com>
2022-02-07 14:08:39 +0500

    feat: added feedback statistics

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,32 @@
 <script>
 	import FeedbackList from "./components/FeedbackList.svelte";
-const feedback = [
-  {
-    id: 1,
-    rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 2,
-    rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-  {
-    id: 3,
-    rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-  },
-]
+	let feedback = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	]
+	$: count = feedback.length
+	$: average = feedback.reduce((prev, {rating}) => prev + rating, 0)/feedback.length;
+const deleteFeedback=(e)=>{
+	const id = e.detail;
+	feedback = feedback.filter(item=>item.id!==id)
+}
 </script>
 
 <main class="container">
-<FeedbackList {feedback} />
+<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 <style>
